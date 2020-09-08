f539d41f22be41b14e76bd1c169d37aa557e4317
david-anders <david-anders@live.com>
2020-09-08 13:22:54 +0300

    #6 refactoring, and added metadata (padding + header).

--- encryptor/encrypt.py
+++ encryptor/encrypt.py
@@ -20,7 +20,6 @@ T = TypeVar("T")
 
 IV_LEN = 2**7 // 8  # 16 Byte long initialization vector for 128 bit AES
 
-
 class EncryptedData:
     """ A Class representing encrypted data that can be shared safely.
     Supplies functions for easy encryption and decryption of data for people that are not very familiar with python or
@@ -48,6 +47,19 @@ class EncryptedData:
         return isinstance(other, self.__class__) and self.iv == other.iv and self.data == other.data
 
     @classmethod
+    def deserialize(cls: Type[T], data: bytes) -> T:
+        """ Deserialize An EncryptedData object.
+        Can be used to read a shared secret.
+
+        :param data: The serialized data representing the EncryptedData object.
+        :return: A EncryptedData object.
+        """
+        iv = data[:IV_LEN]
+        data_start = IV_LEN
+        data = data[data_start:]
+        return cls(data=data, iv=iv)
+
+    @classmethod
     def encrypt_data(cls: Type[T], data: bytes, key: bytes) -> T:
         """ Encrypt data that you want to keep a secret.
 
@@ -56,10 +68,11 @@ class EncryptedData:
         :param key: The key to use to encrypt the data.
         :return: A new EncryptedData object.
         """
+        padded_data = _Metadata().wrap_data(data)
         iv = os.urandom(IV_LEN)  # Initialization Vector with Windows CryptGenRandom()
         cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
         encryptor = cipher.encryptor()
-        enc_data = encryptor.update(data) + encryptor.finalize()
+        enc_data = encryptor.update(padded_data) + encryptor.finalize()
         return cls(data=enc_data, iv=iv)
 
     @classmethod
@@ -85,18 +98,24 @@ class EncryptedData:
         with open(path_in, "rb") as file:
             return cls.deserialize(file.read())
 
-    @classmethod
-    def deserialize(cls: Type[T], data: bytes) -> T:
-        """ Deserialize An EncryptedData object.
-        Can be used to read a shared secret.
-
-        :param data: The serialized data representing the EncryptedData object.
-        :return: A EncryptedData object.
+    def decrypt(self, key) -> bytes:
+        """ Decrypt this EncryptedData object.
+        Decrypt the Data with the ocrrect key to read the secret.
+        :param key: The key to decrypt the data with.
+        :return: The decrypted secret.
         """
-        iv = data[:IV_LEN]
-        data_start = IV_LEN
-        data = data[data_start:]
-        return cls(data=data, iv=iv)
+        cipher = Cipher(algorithms.AES(key), modes.CBC(self.iv), backend=default_backend())
+        decryptor = cipher.decryptor()
+        padded_data = decryptor.update(self.data) + decryptor.finalize()
+
+        return _Metadata().unwrap_data(padded_data)
+
+    def serialize(self) -> bytes:
+        """ Serializes the EncryptedData object to a series of bytes, that can be easily used to share a secret.
+        The other end can easily deserialize the object and with the correct key, encrypt and read the secret.
+        :return: A bytes representation of the encrypted secret.
+        """
+        return self.iv + self.data
 
     def to_file(self, path_out: str):
         """ Write the EncryptedData object to a file for sharing.
@@ -106,23 +125,6 @@ class EncryptedData:
         """
         with open(path_out, "wb") as file:
             file.write(self.serialize())
-
-    def serialize(self) -> bytes:
-        """ Serializes the EncryptedData object to a series of bytes, that can be easily used to share a secret.
-        The other end can easily deserialize the object and with the correct key, encrypt and read the secret.
-        :return: A bytes representation of the encrypted secret.
-        """
-        return self.iv + self.data
-
-    def decrypt(self, key) -> bytes:
-        """ Decrypt this EncryptedData object.
-        Decrypt the Data with the ocrrect key to read the secret.
-        :param key: The key to decrypt the data with.
-        :return: The decrypted secret.
-        """
-        cipher = Cipher(algorithms.AES(key), modes.CBC(self.iv), backend=default_backend())
-        decryptor = cipher.decryptor()
-        return decryptor.update(self.data) + decryptor.finalize()
 
 
 def encrypt_file(in_path: str, key: bytes, out_path: str):
@@ -144,3 +146,83 @@ def decrypt_file(in_path: str, key: bytes, out_path: str):
     :param out_path: The path to which to write the decrypted data.
     """
     EncryptedData.encrypt_file(in_path, key).to_file(out_path)
+
+
+class _Metadata:
+    """ A Class representing Metadata for shared data, including the original data length, as well as checksums and
+    other information. Used to pad the data to a integer multiple of the block size.
+    """
+    _BYTES_RESERVED_FOR_LEN = 4  # Length of the payload is encoded in 4 bytes. Data must be less then 2**32 bytes long.
+    _BYTEORDER = "big"  # Big endian byteorder
+    _HEADER_BLOCKS = 16  # 16 blocks are used for the header itself, not inlcuding the trailer which is padding.
+
+    def __init__(self, block_size: int = 16):
+        """ Instantiate a object that can be used to wrap data with.
+
+        @param block_size: The size of the underlying blocks to which the data should be padded to.
+        """
+        self.block_size = block_size
+
+    def wrap_data(self, data: bytes) -> bytes:
+        """ Wraps the ´data´ with the header and pads it to be a integer multiple of ´block_size´.
+
+        @param data: The original ´data´ to wrap and add padding to.
+        @return: The ´data´ wrapped with the header and padding
+        """
+        return self._generate_header(data) + data + self._generate_padding(len(data))
+
+    def _generate_header(self, data: bytes) -> bytes:
+        assert self.block_size >= 5, "The header requires that the block size is at least 5."
+        field_length = len(data).to_bytes(self._BYTES_RESERVED_FOR_LEN, self._BYTEORDER, signed=False)
+        field_length_hash = checksum(field_length)
+
+        header = bytes()
+        # First block is reserved for length and hash of length.
+        header = header + field_length
+        header = header + b'\x00' * (self.block_size - len(field_length) - len(field_length_hash)) # fill unused
+        header = header + field_length_hash  # last byte of first block is currently the checksum
+
+        # Rest of header (hash of the data) is still unimplemented. fill with 0.
+        header = header + b'\x00' * self.block_size * (self._HEADER_BLOCKS - 1)  # 15 unused blocks. reserved for hash.
+        return header
+
+    def _generate_padding(self, data_len_bytes: int) -> bytes:
+        return os.urandom(self._get_pad_len(data_len_bytes))
+
+    def _get_pad_len(self, data_len_bytes) -> int:
+        unaligned_bytes = data_len_bytes % self.block_size
+        return (self.block_size - unaligned_bytes) % self.block_size
+
+    def _header_parse_data_length(self, wrapped_data: bytes) -> int:
+        received_checksum = bytes([wrapped_data[self.block_size - 1]])
+        expected_checksum = checksum(wrapped_data[:4])
+        if received_checksum != expected_checksum:
+            raise DataIntegrityError(f"The received {received_checksum} and expected {expected_checksum} checksums for "
+                                     f"the length field do not match")
+        return int.from_bytes(wrapped_data[:4], byteorder=self._BYTEORDER, signed=False)
+
+    def unwrap_data(self, wrapped_data: bytes) -> bytes:
+        """ Removes the Metadata (header + padding) from the data, and returns the original data.
+
+        :param wrapped_data: The wrapped data, with length field and checksums and padding
+        :return: The original not wrapped data
+        """
+        data_start = self.block_size * self._HEADER_BLOCKS
+        data_len = self._header_parse_data_length(wrapped_data)
+
+        added_length = self.block_size * self._HEADER_BLOCKS + self._get_pad_len(len(wrapped_data[:data_len]))
+        if data_len != len(wrapped_data) - added_length:
+            raise DataIntegrityError(f"Expected a data length of {data_len} bytes, got {len(wrapped_data)} bytes")
+        return wrapped_data[data_start: data_start + data_len]
+
+
+def checksum(data: bytes) -> bytes:
+    sum_byte = 0
+    for byte in data:
+        sum_byte += byte
+    return bytes([sum_byte % 2**8])
+
+
+class DataIntegrityError(Exception):
+    def __init__(self, message: str = "Data is corrupted"):
+        super(DataIntegrityError, self).__init__(message)
--- encryptor/test_encryptor.py
+++ encryptor/test_encryptor.py
@@ -1,8 +1,9 @@
 import os
 import unittest
 from difflib import SequenceMatcher
+from math import floor, ceil
 
-from encryptor.encrypt import EncryptedData
+from encryptor.encrypt import EncryptedData, _Metadata, DataIntegrityError
 
 
 class TestEncryption(unittest.TestCase):
@@ -11,10 +12,10 @@ class TestEncryption(unittest.TestCase):
         self.test_key = os.urandom(128 // 8)
         self.test_iv = os.urandom(128 // 8)
 
-    def test_encryption_end_decryption_dont_change_data(self):
-        encypted_data = EncryptedData.encrypt_data(self.test_data, self.test_key)
-        decryted_data = encypted_data.decrypt(self.test_key)
-        self.assertEqual(self.test_data, decryted_data)
+    def test_encryption_and_decryption_dont_change_data(self):
+        encrypted_data = EncryptedData.encrypt_data(self.test_data, self.test_key)
+        decrypted_data = encrypted_data.decrypt(self.test_key)
+        self.assertEqual(self.test_data, decrypted_data)
 
     def test_serialize_and_deserialize_are_opposite_operations(self):
         s_data = EncryptedData(data=self.test_data, iv=self.test_iv).serialize()
@@ -36,8 +37,44 @@ class TestEncryption(unittest.TestCase):
         written_data = EncryptedData(data=self.test_data, iv=self.test_iv)
         written_data.to_file("enc_test_temp_file")
         read_data = EncryptedData.from_file("enc_test_temp_file")
-        self.assertEqual(written_data, read_data)
         os.remove("enc_test_temp_file")
+        self.assertEqual(written_data, read_data)
+
+
+class TestPadder(unittest.TestCase):
+    def test_long_padding_is_filling_until_aligned(self):
+        pad_len = 16
+        test_data = b"\xFF" * (pad_len + 1)
+        self.assertEqual(len(_Metadata(pad_len).wrap_data(test_data)) % pad_len, 0)
+
+    def test_short_padding_is_adding_a_new_segment(self):
+        pad_len = 16
+        test_data = b"\xFF" * (pad_len - 1)
+        self.assertEqual(len(_Metadata(pad_len).wrap_data(test_data)) % pad_len, 0)
+
+    def test_data_corruption_raises_exception(self):
+        pad_len = 16
+        data_len = (pad_len * 250)
+        padded_data = _Metadata(pad_len).wrap_data(b"\xFF" * data_len)
+        corrupted_data = padded_data[:data_len] + padded_data[data_len + 1:]
+        self.assertRaises(DataIntegrityError, _Metadata(pad_len).unwrap_data, corrupted_data)
+
+    def test_padding_length_is_calculated_correctly(self):
+        pad_len = 16
+        padder = _Metadata(pad_len)
+        self.assertEqual(padder._get_pad_len(pad_len), 0)
+        self.assertEqual(padder._get_pad_len(pad_len - 1), 1)
+
+    def test_add_padding_and_remove_padding_are_opposite_operations(self):
+        byte = b'1'
+        pad_len = 16
+        padder = _Metadata(pad_len)
+        test_data = [byte * data_length for data_length in range(512)]
+        for test in test_data:
+            with self.subTest(data_len=len(test)):
+                padded_data = padder.wrap_data(test)
+                unpadded_data = padder.unwrap_data(padded_data)
+                self.assertEqual(test, unpadded_data)
 
 
 if __name__ == '__main__':
